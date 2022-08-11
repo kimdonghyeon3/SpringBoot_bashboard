afcd9b98c93eca99cb69ae5d17b92a4d61dd205f
kimdonghyeon <sh2115521@naver.com>
2022-08-11 15:39:48 +0900

    test : answerRepository,questionRepository test

---
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.ll.exam.sbb;
+
+import com.ll.exam.sbb.entity.Answer;
+import com.ll.exam.sbb.entity.Question;
+import com.ll.exam.sbb.repository.AnswerRepository;
+import com.ll.exam.sbb.repository.QuestionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class AnswerRepositoryTest {
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Autowired
+    private AnswerRepository answerRepository;
+    private int lastSampleDataId;
+
+    @BeforeEach
+    void beforeEach() {
+        clearData();
+        createSampleData();
+    }
+
+    private void clearData() {
+        QuestionRepositoryTest.clearData(questionRepository);
+
+        questionRepository.disableForeignKeyChecks();
+        answerRepository.truncate();
+        questionRepository.enableForeignKeyChecks();
+    }
+
+    private void createSampleData() {
+        QuestionRepositoryTest.createSampleData(questionRepository);
+    }
+
+    @Test
+    void 저장() {
+        Question q = questionRepository.findById(2);
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q);
+        a.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a);
+    }
+}
